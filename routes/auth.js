0e919e8bec09e97ccb179b2b33cc25f931d7dcb7
Lamiss Messaoudi <younglamiss@gmail.com>
2021-04-04 13:35:41 +0100

    Refactor code( users & auth controllers)

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,8 @@
-const config = require('config');
-const jwt = require('jsonwebtoken')
 const express = require('express');
 const router = express.Router();
-const _ = require('lodash')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const { User } = require('../models/user')
-const Joi = require('joi'); //  Joi is a class
+const authController = require('../controller/auth');
 
-function validateAuth(req) {
-    const schema = {
-        email: Joi.string().min(3).max(255).required().email(),
-        password: Joi.string().min(3).max(255).required(),
-    }
-    return Joi.validate(req, schema)
-}
-
-router.post('/', async (req, res) => {
-
-    //input validation using joi 
-    const { error } = validateAuth(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message)
-
-    let user = await User.findOne({ email: req.body.email })
-    if (!user) return res.status(400).send("Invalid email or password")
-
-    const validPwd = await bcrypt.compare(req.body.password, user.password)
-    if (!validPwd)
-        return res.status(400).send("Invalid email or password")
-
-    const token = user.generateAuthToken();
-
-    res.send(token)
-})
+router.post('/', authController.Auth)
 
 //private key to create digital signature
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const _ = require('lodash')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const { validateuser, User } = require('../models/user')
-const config = require('config');
-const jwt = require('jsonwebtoken')
-const auth = require('../middleware/auth')
+const userController = require('../controller/users');
 
-router.get('/me', auth, async (req, res) => {
-    const user = await User.findById(req.user._id).select('-password')
-    res.send(user)
-})
+router.get('/me', userController.Me)
 
-router.post('/', async (req, res) => {
-
-    //input validation using joi 
-    const { error } = validateuser(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message)
-
-    let user = await User.findOne({ email: req.body.email })
-    if (user) return res.status(400).send("User already registered")
-
-    user = new User(_.pick(req.body, ['name', 'email', 'password']))
-    //salt is a random string addesd to the pwd before hashing it for more security
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(user.password, salt)
-
-    await user.save();
-    const token = user.generateAuthToken();
-    //returning the token as a header
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
-})
+router.post('/', userController.CreateUser)
 //payload are the data we store insie the token 
 
 module.exports = router;
